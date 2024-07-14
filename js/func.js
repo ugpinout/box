89b4e47bb22a41ef1fcae8da4a13842d3260b713
ugpinout <18599299692@163.com>
2024-07-15 01:15:45 +0800

    第一

--- js/func.js
+++ js/func.js
@@ -1,20 +1,26 @@
- export function createFile(filePath) {
-	plus.io.requestFileSystem(plus.io.PRIVATE_DOC, function(fs) {
-		fs.root.getFile('newfile.txt', { create: true }, function(entry) {
-			console.log('New file created: ' + entry.fullPath);
-			// 在这里可以处理新文件创建后的逻辑
-		}, function(e) {
-			console.log('Failed to create file: ' + e.message);
-		});
-	}, function(e) {
-		console.log('Request filesystem failed: ' + e.message);
-	});
-}
 
-export function get_ku(successCallback,errorCallback){
+
+export function connect_sql(callback){
+	plus.sqlite.openDatabase({
+		name:"data",
+		path:'_doc/data.db',
+		success:function(res){
+			callback(true);
+		},
+		fail:function(e){
+			if(plus.sqlite.isOpenDatabase({name:"data",path:'_doc/data.db'})){
+				callback(true);
+			}else{
+				callback(false);
+			}   
+		}
+	});
+} 
+
+export function select_ku(sql,successCallback,errorCallback){
 	plus.sqlite.selectSql({
 		name: 'data',
-		sql: 'select ids,name from ku',
+		sql: sql,
 		success: function(data){
 			successCallback(data);
 		},
@@ -24,8 +30,34 @@ export function get_ku(successCallback,errorCallback){
 	});
 }
 
+export function get_ku(successCallback,errorCallback){
+	plus.sqlite.selectSql({
+		name: 'data',
+		sql: 'select * from ku',
+		success: function(data){
+			successCallback(data);
+		},
+		fail: function(e){
+			errorCallback(e);
+		}
+	});
+}
+
+export function execute_ku(sqls,successCallback,errorCallback){ // 0 为添加库，//1为删除库，//2为重改名库
+	plus.sqlite.executeSql({
+		name:'data',
+		sql:sqls,
+		success:function(res){
+			successCallback(res);
+		},fail:function(e){
+			errorCallback(e);
+		}
+	});
+}
+
 export function add_new_box_to_list(data_obj){
 	let ulElement = document.querySelector('#box_list');
+	ulElement.innerHTML = '';
 	for (let item in data_obj) {
 		// 创建 li 元素
 		let liElement = document.createElement('li');
@@ -41,7 +73,7 @@ export function add_new_box_to_list(data_obj){
 		imgElement.alt = '';
 		// 创建 div 元素
 		let divElement = document.createElement('div');
-		divElement.setAttribute('id',data_obj[item].ids);
+		divElement.setAttribute('id',data_obj[item].id);
 		divElement.classList.add('mui-media-body');
 		divElement.textContent =data_obj[item].name;
 		 
@@ -54,6 +86,39 @@ export function add_new_box_to_list(data_obj){
 		
 		// 将 li 元素添加到 ul 元素中
 		ulElement.appendChild(liElement);
-		console.log(data_obj[item].ids,data_obj[item].name);
+
 	}
+	li_add_Event();
 }
+
+//给所有li标签添加点击事件
+function li_add_Event(){
+	let timer = null;
+	let startTime = '';
+	let endTime = '';
+	var ulElement = document.getElementById('box_list'); // 获取 ul 元素
+	var liElements = ulElement.querySelectorAll('li.mui-table-view-cell'); // 选择所有类名为 mui-table-view-cell 的 li 元素
+	// mui.toast('这里1');
+	// 循环给每个 li 元素添加触摸事件
+	liElements.forEach(function(li) {
+		li.addEventListener('touchstart', function () {
+			startTime = +new Date()
+			timer = setTimeout(function () { 
+			var lis=li.querySelector('div');
+			// mui.toast(lis.getAttribute('id'));
+			mui('.mui-popover').popover('toggle',document.getElementById(lis.getAttribute('id')));
+			// mui.toast('长按');
+			}, 400);
+		});
+			
+		li.addEventListener('touchend', function () {
+			endTime = +new Date()
+			clearTimeout(timer)
+			if (endTime - startTime < 400) {
+				mui.toast('不是 长按');
+			}
+		}) ;
+	});
+}
+
+
--- pages/main/main.js
+++ pages/main/main.js
@@ -1,24 +1,28 @@
 /*
+自定义的错误代码
 10001：解析 _doc 目录失败,可能没有给权限
 10002：原始数据库文件不存在，有二次修改的可能
 10003：复制数据库失败，可能没有给权限
 10004：打开数据库失败，可能没给全选或者数据库误删。重新打开或重新安装软件
 10005：数据库select无法查询到库表，可能数据建库损坏
-*/
-
-import {createFile,get_ku,add_new_box_to_list} from "../../js/func.js";
+10006：创建新的表失败，名字冲突或者数据库有问题
+10007：获取新表的id失败，可能数据库受损
+*/    
+//导入一些库  
+import {get_ku,add_new_box_to_list,execute_ku,connect_sql,select_ku} from "../../js/func.js";
+//页面加载完以后出发的事件
 window.onload=function(){
-	system_init();
 	document.addEventListener( "plusready", onPlusReady, false );
-	}
+	system_init(); 
+}
 
+//验证本地文件完整性
 function is_local_data(){
 	var wwwPath = '_www/src/';  // www 目录路径
 	var docPath = '_doc/';  // _doc 目录路径
 	var filePath = docPath + 'data.db'; // 目标文件路径
 	plus.io.resolveLocalFileSystemURL(filePath, function(entry){
-		console.log('数据库存在');
- 
+		console.log('数据库存在'); 
 		connect_sql(function(res){
 			if(res){
 				get_ku(function(suc){ 
@@ -29,7 +33,7 @@ function is_local_data(){
 				});
 			}else{
 				mui.toast('未知错误[10004]');
-			} 
+			}  
 		});
 	},
 	function(e) {
@@ -40,6 +44,19 @@ function is_local_data(){
 				var docDirectory = docEntry;
 				var docDirectory = docEntry;
 				copyDbFileToDoc(dbFile, docDirectory);
+				connect_sql(function(res){
+					if(res){
+						get_ku(function(suc){ 
+							add_new_box_to_list(suc);     
+						},function(err){    
+							mui.toast('未知错误[10005]');
+							mui.toast(err.message);   
+						});
+					}else{
+						mui.toast('未知错误[10004]');
+					}  
+				});
+				
 			}, function(e) {
 				mui.toast('未知错误[10001]');
 				console.error('解析 _doc 目录失败：' + e.message);
@@ -48,52 +65,71 @@ function is_local_data(){
 			mui.toast('未知错误[10002]');
 			console.error('解析 data.db 文件失败：' + e.message);
 		}); 
-	
 		function copyDbFileToDoc(dbFile, docDirectory) {
 			dbFile.copyTo(docDirectory, 'data.db', function() {
 				console.log('数据库文件复制成功');
-				
 			}, function(e) {
 				mui.toast('未知错误[10003]');
 				console.error('数据库文件复制失败：' + e.message);
 			});
 		}
 	});
-}
-function connect_sql(callback){
-	plus.sqlite.openDatabase({
-		name:"data",
-		path:'_doc/data.db',
-		success:function(res){
-			callback(true);
-		},
-		fail:function(e){
-			if(plus.sqlite.isOpenDatabase({name:"data",path:'_doc/data.db'})){
-				callback(true);
-			}else{
-				callback(false);
-			}
-		}
-	});
+	
 }
 
+
+//h5+功能初始化完以后触发的函数
 function onPlusReady(){
-is_local_data();
-	document.getElementById('add_new').addEventListener('touchstart',function(){
-		
-	});
+	is_local_data(); 
 	
+	//给 添加盒子 按钮绑定点击事件
+	document.getElementById('add_new').addEventListener('touchstart',
+		function(){ 
+			mui.prompt('','不要写的太多','请输入箱子名',['确定','取消'],
+			function(res){
+				if (res.index == 1){
+					mui.toast('创建新盒子取消');
+				}else{ 
+					var id= new Date().getTime();
+					execute_ku("INSERT INTO ku (name,id) VALUES ('"+res.value+"',"+id+");", 
+					function(res){  
+						select_ku("CREATE TABLE \"" +id+ "\" AS SELECT * FROM demo WHERE 1=0;",
+						function(res){
+							console.log(res);
+							mui.toast("创建成功");
+							get_ku(function(suc){ 
+								add_new_box_to_list(suc);       
+							},function(err){ 
+								mui.toast('未知错误[10005]');
+								mui.toast(err.message);  
+							});  
+						},
+						function(e){
+							console.log(JSON.stringify(e));
+							mui.toast('未知错误[10007]'); 
+						});
+					},function(e){
+						console.log(JSON.stringify(e));
+						mui.toast('未知错误[10006]');  
+					});
+				}	 
+			},'div');
+		});
+	//给 左上角按钮 按钮绑定点击事件 
 	document.getElementById('showSidebarButton').addEventListener('touchstart',function(){
 		mui('.mui-off-canvas-wrap').offCanvas('toggle');
 	},{ passive: false });
+	
 }
-
+//测试函数
 function cliks(){
-	alert("Hello");
+	alert("Hello");   
 	mui.toast('登陆成功');
 }
 
+//初始化一些参数  
 function system_init(){
 	mui('.mui-scroll-wrapper').scroll();
+	
 }
   
